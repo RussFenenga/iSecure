29fb832a561a5af1213146bc1ce9f1c22b9a7d46
RussFenenga <russfenenga@gmail.com>
2016-04-22 22:22:08 -0700

    Initial Commit

---
+++ iSecure/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  iSecure
+//
+//  Created by Russ Fenenga on 4/22/16.
+//  Copyright © 2016 com.TeamWon. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
